953abef95cbc4f2b90e248dcc9615c677e760bf6
sundaram <prathip221@gmail.com>
2025-03-19 12:58:52 +0530

    walkincustomers updated

---
+++ app/Models/WalkinCustomer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class WalkinCustomer extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'customer_id',
+        'branch_id',
+        'customer_enter_time',
+        'customer_out_time',
+        'know_about',
+        'is_purchased',
+        'store_experience_review',
+        'jewellery_review',
+        'pricing_review',
+        'staff_review',
+        'friendly_review',
+        'service_review',
+        'assit_review',
+        'non_purchased_review'
+    ];
+}
--- database/migrations/2025_03_14_111418_create_customers_table.php
+++ database/migrations/2025_03_14_111418_create_customers_table.php
@@ -15,11 +15,7 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('branch_id')->nullable();
-            $table->foreign('branch_id')->references('id')->on('branches');
             $table->string('customer_id')->nullable();
-            $table->time('customer_enter_time')->nullable();
-            $table->time('customer_out_time')->nullable();
             $table->string('name')->nullable();
             $table->string('phone_number')->nullable();
             $table->string('email')->nullable();
@@ -32,16 +28,6 @@ class CreateCustomersTable extends Migration
             $table->foreign('qualfication_id')->references('id')->on('qualifications');
             $table->string('address')->nullable();
             $table->string('pincode')->nullable();
-            $table->integer('know_about')->default(0);
-            $table->integer('is_purchased')->default(0);
-            $table->integer('store_experience_review')->default(0);
-            $table->integer('jewellery_review')->default(0);
-            $table->integer('pricing_review')->default(0);
-            $table->integer('satification_review')->default(0);
-            $table->integer('friendly_review')->default(0);
-            $table->integer('knowledge_review')->default(0);
-            $table->integer('assit_review')->default(0);
-            $table->integer('non-purchased_review')->default(0);
             $table->timestamps();
             $table->softDeletes();
         });
