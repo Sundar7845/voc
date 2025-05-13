2cea27298e0a3330afde4d7a88e151188456ff91
Ravi Kiran <ravi.kiran8844@gmail.com>
2025-05-13 16:25:36 +0530

    added customer details  table

--- resources/views/Backend/customerdetails.blade.php
+++ resources/views/Backend/customerdetails.blade.php
@@ -19,9 +19,30 @@
         </div>
     </div>
 
-    <div class="border-b border-[#C7C7C7] mt-6">
-        <div class="border-b-3 border-[#9D4F2A] max-w-fit pe-10 font-medium pb-2">Previous History</div>
+    <div class="border-b border-[#C7C7C7] border-[#C7C7C7] mt-6">
+        <div class="border-b border-[#C7C7C7]-3 border-[#9D4F2A] max-w-fit pe-10 font-medium pb-2">Previous History</div>
     </div>
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <div class="mt-8">
         <div class="overflow-x-auto rounded-box border border-[#C7C7C7] max-w-lg">
             <table class="table">
@@ -42,9 +63,199 @@
             </table>
         </div>
     </div>
-    <input type="hidden" name="customer_id" id="customer_id" value="{{ $customerDetails->customer_id }}">
+
+
+    <div class="mt-10" x-data="{ openRow: null }">
+       <div class="overflow-x-auto">
+       <table class="min-w-full table-auto border border-[#C7C7C7] text-center">
+            <thead class="bg-black text-white text-sm">
+                <tr>
+                    <th class="px-4 py-3">S.No</th>
+                    <th class="px-4 py-3">DATE OF VISIT</th>
+                    <th class="px-4 py-3">CUSTOMER TYPE</th>
+                    <th class="px-4 py-3">VIEW FEEDBACK</th>
+                    <th class="px-4 py-3">ORDER HISTORY</th>
+                </tr>
+            </thead>
+            <tbody class="text-sm text-gray-700">
+                <!-- Row 1 -->
+                <tr class="border-b border-[#C7C7C7]">
+                    <td class="px-4 py-2">1</td>
+                    <td class="px-4 py-2">28-05-2025</td>
+                    <td class="px-4 py-2">Purchased</td>
+                    <td class="px-4 py-2">Good</td>
+                    <td class="px-4 py-2">
+                        <button class="text-sm text-[#9D4F2A] bg-transparent border border-[#9D4F2A] px-4 py-1 rounded-2xl cursor-pointer"
+                            @click="openRow = openRow === 1 ? null : 1">View Details</button>
+                    </td>
+                </tr>
+
+                <!-- Sub Table -->
+                <tr x-cloak x-show="openRow === 1" class="bg-gray-50">
+                <td colspan="5">
+                        <div class="p-4">
+                            <table class="w-full table-auto text-center text-xs">
+                                <thead class="bg-[#F2EDE4] text-[#313131]">
+                                    <tr>
+                                        <th class="p-2">Branch</th>
+                                        <th class="p-2">Invoice Date</th>
+                                        <th class="p-2">Purchase Location</th>
+                                        <th class="p-2">Article Code</th>
+                                        <th class="p-2">Name</th>
+                                        <th class="p-2">SKU No</th>
+                                        <th class="p-2">Sales ID</th>
+                                        <th class="p-2">Sales Person</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="border-t border-[#E0E0E0]">
+                                        <td class="p-2">Coimbatore</td>
+                                        <td class="p-2">15-06-2025</td>
+                                        <td class="p-2">Jewel One Showroom</td>
+                                        <td class="p-2">Rings</td>
+                                        <td class="p-2">Ladies Ring</td>
+                                        <td class="p-2">RNG000086619</td>
+                                        <td class="p-2">SO/1314/00030440</td>
+                                        <td class="p-2">RE0030</td>
+                                    </tr>
+                                    <tr class="border-t border-[#E0E0E0]">
+                                        <td class="p-2">Coimbatore</td>
+                                        <td class="p-2">15-06-2025</td>
+                                        <td class="p-2">Jewel One Showroom</td>
+                                        <td class="p-2">Rings</td>
+                                        <td class="p-2">Ladies Ring</td>
+                                        <td class="p-2">RNG000086619</td>
+                                        <td class="p-2">SO/1314/00030440</td>
+                                        <td class="p-2">RE0030</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
+                </tr>
+
+
+
+                <!-- Row 2 -->
+                <tr class="border-b border-[#C7C7C7]">
+                    <td class="px-4 py-2">2</td>
+                    <td class="px-4 py-2">28-05-2025</td>
+                    <td class="px-4 py-2">Purchased</td>
+                    <td class="px-4 py-2">Good</td>
+                    <td class="px-4 py-2">
+                    <button class="text-sm text-[#9D4F2A] bg-transparent border border-[#9D4F2A] px-4 py-1 rounded-2xl cursor-pointer"
+                    @click="openRow = openRow === 2 ? null : 2">View Details</button>
+                    </td>
+                </tr>
+
+                <!-- Sub Table Row 2 -->
+                <tr x-cloak x-show="openRow === 2" class="bg-gray-50">
+                <td colspan="5">
+                        <div class="p-4">
+                            <table class="w-full table-auto text-center text-xs">
+                                <thead class="bg-[#F2EDE4] text-[#313131]">
+                                    <tr>
+                                        <th class="p-2">Branch</th>
+                                        <th class="p-2">Invoice Date</th>
+                                        <th class="p-2">Purchase Location</th>
+                                        <th class="p-2">Article Code</th>
+                                        <th class="p-2">Name</th>
+                                        <th class="p-2">SKU No</th>
+                                        <th class="p-2">Sales ID</th>
+                                        <th class="p-2">Sales Person</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="border-t border-[#E0E0E0]">
+                                        <td class="p-2">Coimbatore</td>
+                                        <td class="p-2">15-06-2025</td>
+                                        <td class="p-2">Jewel One Showroom</td>
+                                        <td class="p-2">Rings</td>
+                                        <td class="p-2">Ladies Ring</td>
+                                        <td class="p-2">RNG000086619</td>
+                                        <td class="p-2">SO/1314/00030440</td>
+                                        <td class="p-2">RE0030</td>
+                                    </tr>
+                                    <tr class="border-t border-[#E0E0E0]">
+                                        <td class="p-2">Coimbatore</td>
+                                        <td class="p-2">15-06-2025</td>
+                                        <td class="p-2">Jewel One Showroom</td>
+                                        <td class="p-2">Rings</td>
+                                        <td class="p-2">Ladies Ring</td>
+                                        <td class="p-2">RNG000086619</td>
+                                        <td class="p-2">SO/1314/00030440</td>
+                                        <td class="p-2">RE0030</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
+                </tr>
+
+                <!-- Row 3 -->
+                <tr class="border-b border-[#C7C7C7]">
+                    <td class="px-4 py-2">3</td>
+                    <td class="px-4 py-2">28-05-2025</td>
+                    <td class="px-4 py-2">Purchased</td>
+                    <td class="px-4 py-2">Good</td>
+                    <td class="px-4 py-2">
+                    <button class="text-sm text-[#9D4F2A] bg-transparent border border-[#9D4F2A] px-4 py-1 rounded-2xl cursor-pointer"
+                    @click="openRow = openRow === 3 ? null : 3">View Details</button>
+                    </td>
+                </tr>
+
+                <!-- Sub Table Row 3 -->
+                <tr x-cloak x-show="openRow === 3" class="bg-gray-50">
+                    <td colspan="5">
+                        <div class="p-4">
+                            <table class="w-full table-auto text-center text-xs">
+                                <thead class="bg-[#F2EDE4] text-[#313131]">
+                                    <tr>
+                                        <th class="p-2">Branch</th>
+                                        <th class="p-2">Invoice Date</th>
+                                        <th class="p-2">Purchase Location</th>
+                                        <th class="p-2">Article Code</th>
+                                        <th class="p-2">Name</th>
+                                        <th class="p-2">SKU No</th>
+                                        <th class="p-2">Sales ID</th>
+                                        <th class="p-2">Sales Person</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="border-t border-[#E0E0E0]">
+                                        <td class="p-2">Coimbatore</td>
+                                        <td class="p-2">15-06-2025</td>
+                                        <td class="p-2">Jewel One Showroom</td>
+                                        <td class="p-2">Rings</td>
+                                        <td class="p-2">Ladies Ring</td>
+                                        <td class="p-2">RNG000086619</td>
+                                        <td class="p-2">SO/1314/00030440</td>
+                                        <td class="p-2">RE0030</td>
+                                    </tr>
+                                    <tr class="border-t border-[#E0E0E0]">
+                                        <td class="p-2">Coimbatore</td>
+                                        <td class="p-2">15-06-2025</td>
+                                        <td class="p-2">Jewel One Showroom</td>
+                                        <td class="p-2">Rings</td>
+                                        <td class="p-2">Ladies Ring</td>
+                                        <td class="p-2">RNG000086619</td>
+                                        <td class="p-2">SO/1314/00030440</td>
+                                        <td class="p-2">RE0030</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+       </div>
+    </div>
+
+
+    <!-- <input type="hidden" name="customer_id" id="customer_id" value="{{ $customerDetails->customer_id }}">
     <div class="mt-8 max-h-[500px] overflow-auto border border-[#C7C7C7] rounded-box">
-        <table id="salesReportTable" class="min-w-[1200px] w-full text-sm text-left border-collapse">
+        <table id="salesReportTable" class="min-w-[1200px] w-full text-sm text-center border-collapse">
             <thead class="bg-black">
                 <tr>
                     <th class="px-4 py-2">Branch</th>
@@ -87,10 +298,10 @@
                 </tr>
             </thead>
             <tbody>
-                <!-- Your rows go here -->
             </tbody>
         </table>
-    </div>
+
+    </div> -->
 
 
 </section>
